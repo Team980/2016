104f5b6ddfd337b63dd6b77c353d0aff79eb9f00
Luke Myers <19lmyers@gmail.com>
2016-01-23 11:22:15 -0800

    Add sample code for 2015 Robot
    This sample code uses the twist joystick and the RobotDrive class to
    easily implement a robot drive system for the 2015 robot.
    This is intended to be forked and used to develop test systems for
    vision and motors.

---
+++ src/Robot.cpp
@@ -0,0 +1,59 @@
+#include "WPILib.h"
+
+class Robot: public IterativeRobot
+{
+	RobotDrive myRobot; // robot drive system
+	Joystick stick; // only joystick
+	LiveWindow *lw;
+	int autoLoopCounter;
+
+public:
+	Robot() :
+		myRobot(8, 9),	// these must be initialized in the same order
+		stick(0),		// as they are declared above.
+		lw(LiveWindow::GetInstance()),
+		autoLoopCounter(0)
+	{
+		myRobot.SetExpiration(0.1);
+
+		//The robot was wired backwards
+		//Defaults to rear when using two drives
+		myRobot.SetInvertedMotor(RobotDrive::kRearLeftMotor, true);
+		myRobot.SetInvertedMotor(RobotDrive::kRearRightMotor, true);
+	}
+
+private:
+	void AutonomousInit()
+	{
+		autoLoopCounter = 0;
+	}
+
+	void AutonomousPeriodic()
+	{
+		if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
+		{
+			myRobot.Drive(-0.5, 0.0); 	// drive forwards half speed
+			autoLoopCounter++;
+			} else {
+			myRobot.Drive(0.0, 0.0); 	// stop robot
+		}
+	}
+
+	void TeleopInit()
+	{
+
+	}
+
+	void TeleopPeriodic()
+	{
+		//Use Z axis (twist) for rotation
+		myRobot.ArcadeDrive(stick, 1, stick, 2, true); // drive with arcade style (use right stick)
+	}
+
+	void TestPeriodic()
+	{
+		lw->Run();
+	}
+};
+
+START_ROBOT_CLASS(Robot)
