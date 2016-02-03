65d62dc5e3d984af969987f5709c6107b6cb235c
Luke Myers <19lmyers@gmail.com>
2016-02-02 19:33:31 -0800

    2/2/16 - Fix pointer errors

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -5,9 +5,8 @@
 class Robot: public IterativeRobot
 {
 private:
-	RobotDrive myRobot; // robot drive system
-	Joystick stick; // only joystick
-	LiveWindow *lw;
+	RobotDrive *myRobot; // robot drive system
+	Joystick *stick; // only joystick
 
 	CameraSystem cameraSystem; //Camera system
 
@@ -33,14 +32,14 @@ private:
 	void TeleopPeriodic()
 	{
 		//Use Z axis (twist) for rotation
-		myRobot.ArcadeDrive(stick, 1, stick, 2, true); // drive with arcade style (use right stick)
+		myRobot->ArcadeDrive(stick, 1, stick, 2, true); // drive with arcade style (use right stick)
 
 		cameraSystem.Scan();
 	}
 
 	void TestPeriodic()
 	{
-		lw->Run();
+
 	}
 
 public:
@@ -48,25 +47,23 @@ public:
 	{
 		myRobot = new RobotDrive(8,9);
 		stick = new Joystick(0);
-		lw = new LiveWindow(LiveWindow::GetInstance());
 
 		cameraSystem = new CameraSystem();
 
-		myRobot.SetExpiration(0.1);
+		myRobot->SetExpiration(0.1);
 
 		//The robot was wired backwards
 		//Defaults to rear when using two drives
-		myRobot.SetInvertedMotor(RobotDrive::kRearLeftMotor, true);
-		myRobot.SetInvertedMotor(RobotDrive::kRearRightMotor, true);
+		myRobot->SetInvertedMotor(RobotDrive::kRearLeftMotor, true);
+		myRobot->SetInvertedMotor(RobotDrive::kRearRightMotor, true);
 	}
 
 	~Robot ()
 	{
-		~myRobot();
-		~stick();
-		~lw();
+		delete myRobot;
+		delete stick;
 
-		~cameraSystem();
+		delete cameraSystem;
 	}
 
 };
