faea45ad8b97d5c9d376efa65f2ac58b69860097
Luke Myers <19lmyers@gmail.com>
2016-02-02 18:20:13 -0800

    2/2/16 - Update Robot.cpp to use RobotTemplate

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -2,41 +2,24 @@
 
 class Robot: public IterativeRobot
 {
+private:
 	RobotDrive myRobot; // robot drive system
 	Joystick stick; // only joystick
 	LiveWindow *lw;
-	int autoLoopCounter;
 
-public:
-	Robot() :
-		myRobot(8, 9),	// these must be initialized in the same order
-		stick(0),		// as they are declared above.
-		lw(LiveWindow::GetInstance()),
-		autoLoopCounter(0)
+	void RobotInit()
 	{
-		myRobot.SetExpiration(0.1);
 
-		//The robot was wired backwards
-		//Defaults to rear when using two drives
-		myRobot.SetInvertedMotor(RobotDrive::kRearLeftMotor, true);
-		myRobot.SetInvertedMotor(RobotDrive::kRearRightMotor, true);
 	}
 
-private:
 	void AutonomousInit()
 	{
-		autoLoopCounter = 0;
+
 	}
 
 	void AutonomousPeriodic()
 	{
-		if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
-		{
-			myRobot.Drive(-0.5, 0.0); 	// drive forwards half speed
-			autoLoopCounter++;
-			} else {
-			myRobot.Drive(0.0, 0.0); 	// stop robot
-		}
+
 	}
 
 	void TeleopInit()
@@ -53,6 +36,28 @@ private:
 	void TestPeriodic()
 	{
 		lw->Run();
+	}
+
+public:
+	Robot()
+	{
+		myRobot = new RobotDrive(8,9);
+		stick = new Joystick(0);
+		lw = new LiveWindow(LiveWindow::GetInstance());
+
+		myRobot.SetExpiration(0.1);
+
+		//The robot was wired backwards
+		//Defaults to rear when using two drives
+		myRobot.SetInvertedMotor(RobotDrive::kRearLeftMotor, true);
+		myRobot.SetInvertedMotor(RobotDrive::kRearRightMotor, true);
+	}
+
+	~Robot()
+	{
+		~myRobot();
+		~stick();
+		~lw();
 	}
 };
 
