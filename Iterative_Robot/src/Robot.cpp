a54f7e17168d1f9cc1b201b7337edfa158930218
Bonnie <bonnie@zygoncommand.local>
2016-03-01 20:24:50 -0800

    Changed status reporting to network tables

--- Iterative_Robot/src/Robot.cpp
+++ Iterative_Robot/src/Robot.cpp
@@ -22,6 +22,9 @@ private:
 	CANTalon *armMotor;
 	PIDController *armPid;
 
+	//Network Tables
+	std::shared_ptr<NetworkTable> dataTablePtr;
+
 	void RobotInit()
 	{
 		armPid->SetSetpoint(armUpPosition);
@@ -50,8 +53,10 @@ private:
 			myRobot->Drive(autoSpeed, autoCurve);
 
 		//status
-		std::cout << "leftEnc: " << (autoLeftDistInvert*currentDistLeft) << std::endl;
-		std::cout << "rightEnc: " << (autoRightDistInvert*currentDistRight) << std::endl;
+		//std::cout << "leftEnc: " << (autoLeftDistInvert*currentDistLeft) << std::endl;
+		//std::cout << "rightEnc: " << (autoRightDistInvert*currentDistRight) << std::endl;
+		dataTablePtr ->PutNumber("signedLeftEncDist", autoLeftDistInvert*currentDistLeft);
+		dataTablePtr ->PutNumber ("signedRightEncDist", autoRightDistInvert*currentDistRight);
 	}
 
 	void TeleopInit()
@@ -62,10 +67,6 @@ private:
 
 	void TeleopPeriodic()
 	{
-		//status
-		std::cout << "leftEnc: " << leftDriveEnc->GetDistance() << std::endl;
-		std::cout << "rightEnc: " << rightDriveEnc->GetDistance() << std::endl;
-
 		//drive
 		myRobot->ArcadeDrive(driveStick, Joystick::kYAxis, driveStick, Joystick::kZAxis, true);
 
@@ -103,6 +104,17 @@ private:
 		{
 			armPid->SetSetpoint(armUpPosition);
 		}
+
+		//status
+		//std::cout << "leftEnc: " << leftDriveEnc->GetDistance() << std::endl;
+		//std::cout << "rightEnc: " << rightDriveEnc->GetDistance() << std::endl;
+		dataTablePtr ->PutNumber("LeftEncDist", leftDriveEnc->GetDistance());
+		dataTablePtr ->PutNumber ("RightEncDist", rightDriveEnc->GetDistance());
+		dataTablePtr ->PutBoolean("rollerInOn", rollerInOn);
+		dataTablePtr ->PutBoolean("photoSwitch", photoSwitch);
+		dataTablePtr ->PutNumber("armPidSetpoint", armPid ->GetSetpoint());
+		dataTablePtr ->PutNumber("armPidError", armPid ->GetError());
+		dataTablePtr ->PutNumber("armPot", armPot ->GetVoltage());
 	}
 
 	void TestPeriodic()
